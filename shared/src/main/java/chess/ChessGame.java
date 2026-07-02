a6547816fb5f3282f957c5d4f32d6def7c7fe55d
em81d <degiulioemeline@gmail.com>
2026-07-02 15:53:42 -0600

    setup and equals/hashcode

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -1,6 +1,9 @@
 package chess;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.Objects;
 
 /**
  * A class that can manage a chess game, making moves on a board
@@ -9,6 +12,8 @@ import java.util.Collection;
  * signature of the existing methods.
  */
 public class ChessGame {
+
+    private ChessBoard chessboard;
 
     public ChessGame() {
 
@@ -46,7 +51,13 @@ public class ChessGame {
      * startPosition
      */
     public Collection<ChessMove> validMoves(ChessPosition startPosition) {
-        throw new RuntimeException("Not implemented");
+        Collection<ChessMove> potentialMoves = chessboard.getPiece(startPosition).pieceMoves(chessboard, startPosition);
+        Collection<ChessMove> allValidMoves = new ArrayList<>();
+        for (ChessMove move : potentialMoves) {
+            //add to valid moves if doesn't leave own king in check
+        }
+
+        return allValidMoves;
     }
 
     /**
@@ -96,7 +107,7 @@ public class ChessGame {
      * @param board the new board to use
      */
     public void setBoard(ChessBoard board) {
-        throw new RuntimeException("Not implemented");
+        chessboard = board;
     }
 
     /**
@@ -105,6 +116,28 @@ public class ChessGame {
      * @return the chessboard
      */
     public ChessBoard getBoard() {
-        throw new RuntimeException("Not implemented");
+        return chessboard;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ChessGame chessGame = (ChessGame) o;
+        return Objects.equals(chessboard, chessGame.chessboard);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(chessboard);
+    }
+
+    @Override
+    public String toString() {
+        return "ChessGame{" +
+                "chessboard=" + chessboard +
+                '}';
     }
 }
