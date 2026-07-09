7d31f5ccf45703ccd5fc71321ad4c797cb5d8983
em81d <degiulioemeline@gmail.com>
2026-07-09 13:46:32 -0600

    implemented User DAO interface and memory

---
+++ server/src/main/java/dataaccess/AuthDAO.java
@@ -0,0 +1,8 @@
+package dataaccess;
+
+public interface AuthDAO {
+
+//    createAuth: Create a new authorization.
+//    getAuth: Retrieve an authorization given an authToken.
+//    deleteAuth: Delete an authorization so that it is no longer valid.
+}
---
+++ server/src/main/java/dataaccess/GameDAOMemory.java
@@ -0,0 +1,10 @@
+package dataaccess;
+
+public class GameDAOMemory implements GameDAO {
+
+/*
+            clear: A method for clearing all data from the database. This is used during testing.
+
+
+ */
+}
