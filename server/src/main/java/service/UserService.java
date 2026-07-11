f42093e25be562bc91e4025bf8361281f499446b
em81d <degiulioemeline@gmail.com>
2026-07-10 21:14:27 -0600

    first shot implementing register endpoint

--- server/src/main/java/service/UserService.java
+++ server/src/main/java/service/UserService.java
@@ -1,14 +1,32 @@
 package service;
-
+import dataaccess.*;
+import model.UserData;
 import service.RR.*;
 
 public class UserService {
 
-    public RegisterResult register(RegisterRequest registerRequest) {
-        return new RegisterResult("myUsername", "myAuth", 200);
+    public RegisterResult register(RegisterRequest registerRequest)  { //throws AlreadyTakenException {
+        UserDAO userDao = new UserDAOMemory();
+        AuthDAO authDao = new AuthDAOMemory();
+        String username = registerRequest.username();
+        String password = registerRequest.password();
+        String email = registerRequest.email();
+        //right now the try works if the username can be found, so the catch is what is supposed to happen - kinda funky?
+        try {
+            userDao.getUser(username);
+//            throw new AlreadyTakenException("that username already exists!");
+            return new RegisterResult(username, null, 403);
+        }
+        catch (Exception e) {
+            userDao.createUser(new UserData(username, password, email));
+            String authToken = authDao.createAuth(username);
+            return new RegisterResult(username, authToken, 200);
+        }
+
     }
     public LoginResult login(LoginRequest loginRequest) {
-        return new LoginResult("myUsername", "myAuth", 200);
+        return new LoginResult("u", "a", 200);
     }
     public void logout(LogoutRequest logoutRequest) {}
 }
+
