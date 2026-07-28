ed02e2ef3c41854abe61615f61a1215865751c39
em81d <degiulioemeline@gmail.com>
2026-07-28 12:04:40 -0600

    switched to use sql database

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -21,9 +21,9 @@ public class Server {
 
     public Server() {
 
-        userDao = new UserDAOMemory();
-        gameDao = new GameDAOMemory();
-        authDao = new AuthDAOMemory();
+        userDao = new UserDAOsql();
+        gameDao = new GameDAOsql();
+        authDao = new AuthDAOsql();
 
 
         userService = new UserService(userDao, authDao);
